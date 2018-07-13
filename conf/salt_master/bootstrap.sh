59d5f3c80a724077582a49e0fbb1c229d1655916
Daniel Murphy <danhatesnumbers@gmail.com>
2018-07-13 18:45:02 +0100

    Hopefully fixed file copy in bootstrap script for salt master

--- conf/salt_master/bootstrap.sh
+++ conf/salt_master/bootstrap.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/DanHatesNumbers/AWSLab
-sudo cp -r AWSLab/salt/files/* /var/salt/base/
-sudo cp -r AWSLab/salt/state/* /var/salt/base/
+sudo cp AWSLab/salt/files/* /var/salt/base/
+sudo cp AWSLab/salt/state/* /var/salt/base/
