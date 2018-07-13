114453755e19075546808e6730f51e78ae3b4fb4
Daniel Murphy <danhatesnumbers@gmail.com>
2018-07-13 19:02:46 +0100

    Updated bootstrap script to handle the AWSLab folder already existing

--- conf/salt_master/bootstrap.sh
+++ conf/salt_master/bootstrap.sh
@@ -1,3 +1,7 @@
-git clone https://github.com/DanHatesNumbers/AWSLab
+if [ ! -d "AWSLab" ]; then
+    git clone https://github.com/DanHatesNumbers/AWSLab
+else
+    cd AWSLab && git pull && cd ..
+fi
 sudo cp AWSLab/salt/files/* /var/salt/base/
 sudo cp AWSLab/salt/state/* /var/salt/base/
