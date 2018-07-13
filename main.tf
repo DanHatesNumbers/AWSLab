bfbc7d04d593041073fbcbb2b36bf448afefe5d2
Daniel Murphy <danhatesnumbers@gmail.com>
2018-07-13 17:05:24 +0100

    Added provisioning step to both hosts to install sudo

--- main.tf
+++ main.tf
@@ -108,6 +108,7 @@ resource "aws_instance" "salt_master" {
 
     provisioner "remote-exec" {
         inline = [
+            "su -l root -c 'pkg install sudo'",
             "sudo pkg install py27-salt",
             "sudo mv /tmp/master.conf /usr/local/etc/salt/master",
             "sudo mkdir -p /var/salt/base",
@@ -174,6 +175,7 @@ resource "aws_instance" "web" {
 
     provisioner "remote-exec" {
         inline = [
+            "su -l root -c 'pkg install sudo'",
             "sudo pkg install py27-salt",
             "sudo mv /tmp/minion.conf /etc/salt/minion",
             "sudo sysrc salt_minion_enable=\"YES\"",
