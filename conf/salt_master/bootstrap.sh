682b936d89c135443de0890bd59e95dd083a1ec2
Daniel Murphy <danhatesnumbers@gmail.com>
2018-07-13 22:27:09 +0100

    Split bootstrap pull commands over multiple lines

--- conf/salt_master/bootstrap.sh
+++ conf/salt_master/bootstrap.sh
@@ -2,7 +2,9 @@ if [ ! -d "AWSLab" ]
 then
     git clone https://github.com/DanHatesNumbers/AWSLab
 else
-    cd AWSLab && git pull && cd ..
+    cd AWSLab
+    git pull
+    cd ..
 fi
 sudo cp AWSLab/salt/files/* /var/salt/base/
 sudo cp AWSLab/salt/state/* /var/salt/base/
